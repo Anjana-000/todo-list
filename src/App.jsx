ce27e5db9035bd9e671725e3cd6d202ad8420c2c
Anjana S <24mca26@mgits.ac.in>
2025-07-02 15:40:35 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Input from './components/input';
+import Card from './components/Card';
+import Tasks from './components/task';
+import './App.css'
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import About from './components/about';
+import Navbar from './components/navbar';
+function App() {
+  
+
+  return (
+    <>
+<BrowserRouter>
+      <div className="App">
+       <header>
+        <h1>
+       MY REACT APP
+        </h1>
+        <h2>
+          This is my first Application
+        </h2>
+       </header>
+       <Navbar>
+        
+       </Navbar>
+       <main>
+        <Routes>
+          <Route path="/" element={<Tasks />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+       
+       </main>
+      </div>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/task.jsx
@@ -0,0 +1,73 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Card from "./Card";
+import Input from "./input";
+const Tasks = () => {
+  const initial_task = [
+
+  ];
+  const [tasks, setTasks] = useState(initial_task);
+  const addTaskHandler = async (newTask) => {
+    let newTaskObj = {
+      task_id: Math.random(),
+      task_name: newTask
+
+    };
+    const response = await fetch("https://todo-backend-ke6w.onrender.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+
+      },
+      body: JSON.stringify(newTaskObj),
+    })
+    if (response.status === 201) {
+      getTasks();
+      alert("NEW TASKS ADDED");
+    }
+    else {
+      alert("couldnt add the task");
+    }
+  }
+
+  const deleleTaskHandler = async (taskId) => {
+    const response = await fetch("https://todo-backend-ke6w.onrender.com" + taskId, {
+      method: "DELETE"
+    })
+    if (response.status === 200) {
+      getTasks();
+      alert("Task deleted")
+    } else {
+      alert("Failed to delete")
+    }
+  }
+
+  const getTasks = async () => {
+    const response = await fetch("https://todo-backend-ke6w.onrender.com");
+    const taskList = await response.json();
+    console.log(taskList)
+    setTasks(taskList)
+  }
+  useEffect(() => {
+    getTasks();
+  },
+    [])
+
+  return (
+    <div id="tasks">
+      <Input onAddTask={addTaskHandler} />
+
+      {
+        tasks.map((tk) => (
+
+          <Card task={tk}
+            onDeleteTask={deleleTaskHandler} />
+
+        ))
+      }
+
+    </div>
+  )
+}
+
+export default Tasks;
